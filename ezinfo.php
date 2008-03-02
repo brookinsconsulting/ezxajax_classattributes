fe70859bd7e2ef5edba69a2eed22ece58537af92
Brookins Consulting <info@brookinsconsulting.com>
2008-03-02 08:10:38 +0000

    Updated: Renamed extension internals and updated documentation

---
+++ ezinfo.php
@@ -0,0 +1,20 @@
+<?php
+class ezxajax_classattributesInfo
+{
+    function info()
+    {
+        return array(
+            'Name' => "<a href='http://projects.ez.no/ezxajax_classattributes'>ezxajax_classattributes</a>",
+            'Version' => "1.1",
+            'Copyright' => "Copyright (C) 2006 SCK-CEN",
+            'Author' => "Kristof Coomans",
+            'License' => "GNU General Public License",
+            'Depends on the following third-party software' => array( 'Name' => 'xajax',
+                                                                      'Version' => '0.5 beta',
+                                                                      'License' => 'GNU Lesser General Public License',
+                                                                      'More information' => 'http://www.xajaxproject.org/'
+                                                                     )
+        );
+    }
+}
+?>
